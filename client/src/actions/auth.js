03f484cd4853373bd9f39897e70ad00fde02f528
Antonio Ćorluka <32845849+corlukantonio@users.noreply.github.com>
2019-08-07 18:51:18 +0200

    Everything is fixed
    Everything is fixed

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -45,14 +45,13 @@ export const register = ({ name, email, password }) => async dispatch => {
       type: REGISTER_SUCCESS,
       payload: res.data
     });
-
-    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
     if (errors) {
       errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
     }
+
     dispatch({
       type: REGISTER_FAIL
     });
