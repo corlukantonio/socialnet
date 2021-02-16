5cbc9821c0cc2772a9c392d5b262b2606f7dd7f0
Antonio Ćorluka <corlukantonio@gmail.com>
2021-02-16 02:15:51 +0100

    Update reinstall-node-modules.sh
    Update reinstall-node-modules.sh.

--- shell-scripts/reinstall-node-modules.sh
+++ shell-scripts/reinstall-node-modules.sh
@@ -1,12 +1,28 @@
-# Go to the root folder of the project
+dir_path="./node_modules/"
+
+rm_rf_directory()
+{
+  if [ -d $1 ] # Check if directory exists
+  then
+    echo "Directory $1 is being removed..."
+    rm -rf $1
+  else
+    echo "Directory $1 doesn't exist."
+  fi
+}
+
+# Go to the root directory of the project
 cd ..
 
-# Reinstall server node_modules
-rm -rf ./node_modules/
+rm_rf_directory $dir_path
 npm install
 
+# Go to the client directory
 cd ./client/
 
-# Reinstall client node_modules
-rm -rf ./node_modules/
+rm_rf_directory $dir_path
 npm install
+
+# Go back to the shell-scripts directory
+cd ..
+cd ./shell-scripts/
